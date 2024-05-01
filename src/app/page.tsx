898b07348884d9583bd95b2246c83732d08142f8
Guilherme Garcia <garciaguig@gmail.com>
2024-05-01 17:14:49 -0300

    feat: Add botão para abrir nova reclamação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@mui/material";
+
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <Button
+        variant="contained"
+        size="large"
+        color="secondary"
+        sx={{ fontWeight: "bold" }}
+      >
+        + Abrir Reclamação
+      </Button>
     </main>
   );
 }
