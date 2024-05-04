b3224740870d61c9f8262325689fc59050a2b5d1
Guilherme Garcia <garciaguig@gmail.com>
2024-05-04 18:54:31 -0300

    feat: Implementa método 'requestLogin' para requisição de login ao backend

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,14 +1,21 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   headers: {
-    Accept: 'application/json;charset=UTF-8',
+    Accept: "application/json;charset=UTF-8",
   },
-})
+});
 
 export const requestData = async (endpoint: string) => {
-  const { data } = await instance.get(endpoint)
+  const { data } = await instance.get(endpoint);
 
-  return data
-}
+  return data;
+};
+
+export const requestLogin = async (email: string, password: string) => {
+  const body = { email, password };
+  const { data } = await instance.post("/users/auth/login", body);
+
+  return data;
+};
