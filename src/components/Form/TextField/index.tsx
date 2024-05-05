6f8fe54e8a3d14fe02962a224b533c4cb9ddb085
Guilherme Garcia <garciaguig@gmail.com>
2024-05-05 11:29:16 -0300

    fix: Remove prop desnecessária no componente TextField

--- src/components/Form/TextField/index.tsx
+++ src/components/Form/TextField/index.tsx
@@ -1,15 +1,15 @@
-import * as M from "@mui/material";
-import { useFormContext } from "react-hook-form";
-import { TextFieldProps } from "../utils/types";
+import * as M from '@mui/material';
+import { useFormContext } from 'react-hook-form';
+import { TextFieldProps } from '../utils/types';
 
 export default function TextField(props: TextFieldProps) {
-  const { label, name } = props;
+  const { name } = props;
   const {
     register,
     formState: { errors },
   } = useFormContext();
 
-  const errorMessage = errors[name]?.message as String;
+  const errorMessage = errors[name]?.message as string;
 
   return (
     <M.TextField
