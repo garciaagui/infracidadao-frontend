a7deed328700be7d340c4bdf0031b6c0172946ea
Guilherme Garcia <garciaguig@gmail.com>
2024-05-05 11:26:09 -0300

    feat: Implementa componente Notification em Login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,19 @@
-"use client";
+'use client';
 
-import { TextField } from "@/components/Form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@mui/material";
-import { signIn, SignInResponse } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { FormProvider, useForm } from "react-hook-form";
-import { loginSchema } from "./utils/schemas";
-import { LoginType } from "./utils/types";
+import { TextField } from '@/components/Form';
+import Notification from '@/components/Notification';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@mui/material';
+import { signIn, SignInResponse } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { NOTIFICATION_INITIAL_STATE } from './utils/constants';
+import { loginSchema } from './utils/schemas';
+import { LoginType } from './utils/types';
 
 export default function Login() {
+  const [notification, setNotification] = useState(NOTIFICATION_INITIAL_STATE);
   const router = useRouter();
   const loginForm = useForm<LoginType>({
     resolver: zodResolver(loginSchema),
@@ -17,8 +21,12 @@ export default function Login() {
 
   const { handleSubmit } = loginForm;
 
+  const closeNotification = () => {
+    setNotification((prevState) => ({ ...prevState, isOpen: false }));
+  };
+
   const login = async (data: LoginType) => {
-    const response = (await signIn("credentials", {
+    const response = (await signIn('credentials', {
       ...data,
       redirect: false,
     })) as SignInResponse;
@@ -26,9 +34,10 @@ export default function Login() {
     const { ok, error } = response;
 
     if (ok) {
-      router.push("/");
+      setNotification({ isOpen: true, message: 'Login bem-sucedido!', result: 'success' });
+      router.push('/');
     } else if (!ok && error) {
-      console.error(error);
+      setNotification({ isOpen: true, message: error, result: 'error' });
     }
   };
 
@@ -44,6 +53,7 @@ export default function Login() {
           </Button>
         </form>
       </FormProvider>
+      <Notification closeNotification={closeNotification} {...notification} />
     </main>
   );
 }
