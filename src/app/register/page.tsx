4be03fcd2af6d3a86fac23682d1e49068fb6f6e7
Guilherme Garcia <garciaguig@gmail.com>
2024-05-12 00:44:01 -0300

    feat: Implementa lógica de registro de usuários em Register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import { TextField } from '@/components/Form';
+import { requestUserCreation } from '@/services/axios';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Button from '@mui/material/Button';
-import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { FormProvider, useForm } from 'react-hook-form';
 import { registerSchema } from './utils/schemas';
 import { RegisterType } from './utils/types';
@@ -14,8 +18,19 @@ export default function Register() {
 
   const { handleSubmit } = registerForm;
 
-  const register = () => {
-    console.log('Registro');
+  const register = async (data: RegisterType) => {
+    try {
+      await requestUserCreation(data);
+      await signIn('credentials', {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      });
+
+      router.push('/home');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -23,6 +38,7 @@ export default function Register() {
       <h1>Cadastro de usuários</h1>
       <FormProvider {...registerForm}>
         <form onSubmit={handleSubmit(register)} noValidate>
+          <TextField label="Nome" name="name" type="text" />
           <TextField label="E-mail" name="email" type="email" />
           <TextField label="Senha" name="password" type="password" />
           <Button type="submit" variant="contained" color="primary" size="large">
