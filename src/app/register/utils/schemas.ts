6c46fb9dfaf49c27939569d6a56b883ced90bd82
Guilherme Garcia <garciaguig@gmail.com>
2024-05-12 00:42:40 -0300

    fix: Add 'name' ao schema de criação de usuários

--- src/app/register/utils/schemas.ts
+++ src/app/register/utils/schemas.ts
@@ -1,6 +1,15 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
+  name: z
+    .string()
+    .min(10, {
+      message: 'O nome precisa ter no mínimo 10 caracteres',
+    })
+    .max(255, {
+      message: 'O nome pode ter no máximo 255 caracteres',
+    })
+    .toLowerCase(),
   email: z
     .string()
     .min(1, {
