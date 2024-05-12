62621a690f0f067d2db47f2be05ba49d8337a8b1
Guilherme Garcia <garciaguig@gmail.com>
2024-05-12 10:21:38 -0300

    feat: Add link para Register na página de login

--- src/app/auth/login/styles.tsx
+++ src/app/auth/login/styles.tsx
@@ -1,7 +1,8 @@
-import * as M from '@mui/material';
+import * as Mui from '@mui/material';
+import NextLink from 'next/link';
 import styled from 'styled-components';
 
-export const Button = styled(M.Button)`
+export const Button = styled(Mui.Button)`
   align-self: center;
   font-weight: bold;
 `;
@@ -13,9 +14,13 @@ export const Form = styled.form`
   width: 50%;
 `;
 
+export const Link = styled(NextLink)`
+  font-weight: bold;
+`;
+
 export const Main = styled.main`
   display: grid;
-  grid-template-rows: auto auto;
+  grid-template-rows: auto auto auto;
   justify-items: center;
   margin: 4rem auto;
   row-gap: 2rem;
