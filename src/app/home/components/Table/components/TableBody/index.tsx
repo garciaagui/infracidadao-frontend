a73f24f00a79138f363e970c4fbf17ef1c2ab12f
Guilherme Garcia <garciaguig@gmail.com>
2024-05-15 12:50:24 -0300

    refactor: Implementa StatusIcon na tablea de Home page

--- src/app/home/components/Table/components/TableBody/index.tsx
+++ src/app/home/components/Table/components/TableBody/index.tsx
@@ -1,4 +1,5 @@
 import * as Mui from '@mui/material';
+import StatusChip from '../../../StatusChip';
 import { TableBodyProps } from '../../utils/types';
 import * as Styled from './styles';
 
@@ -7,12 +8,14 @@ export default function TableBody({ data, handleModal }: TableBodyProps) {
     <Mui.TableBody>
       {data.map((item) => (
         <Styled.TableRow key={item.id} onClick={() => handleModal(Number(item.id))}>
-          <Styled.IdTableCell width={'5%'}>#{item.id}</Styled.IdTableCell>
+          <Mui.TableCell width={'5%'} sx={{ color: 'gray' }}>
+            #{item.id}
+          </Mui.TableCell>
           <Mui.TableCell width={'30%'}>{item.title}</Mui.TableCell>
           <Mui.TableCell width={'10%'}>{item.createdAt}</Mui.TableCell>
-          <Styled.StatusTableCell $value={item.status} width={'15%'}>
-            <p>{item.status}</p>
-          </Styled.StatusTableCell>
+          <Mui.TableCell width={'15%'}>
+            <StatusChip status={item.status} size="medium" />
+          </Mui.TableCell>
           <Mui.TableCell width={'15%'}>{item.neighborhood}</Mui.TableCell>
           <Mui.TableCell width={'25%'}>{item.street}</Mui.TableCell>
         </Styled.TableRow>
