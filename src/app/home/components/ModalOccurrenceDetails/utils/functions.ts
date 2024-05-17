aa138c6930f98d901156286a26a88a5b2524054a
Guilherme Garcia <garciaguig@gmail.com>
2024-05-17 15:06:18 -0300

    feat: Implementa método 'generateReplyFormData' p/ gerar FormData em 'ModalOccurrenceReply'

--- src/app/home/components/ModalOccurrenceDetails/utils/functions.ts
+++ src/app/home/components/ModalOccurrenceDetails/utils/functions.ts
@@ -1,9 +1,31 @@
 import { formatDate } from '@/app/home/utils/functions';
 import { OccurrenceType } from '@/app/home/utils/types';
+import { CreateReplyType } from './types';
 
 export const formatOccurrenceData = (data: OccurrenceType): OccurrenceType => {
   return {
     ...data,
     createdAt: formatDate(data.createdAt),
   };
+};
+
+export const generateReplyFormData = (
+  replyData: CreateReplyType,
+  occurrenceId: number,
+  userId: string,
+): FormData => {
+  const formData = new FormData();
+
+  formData.set('description', replyData.description);
+  formData.set('occurrenceId', String(occurrenceId));
+  formData.set('userId', userId);
+
+  console.log(replyData.imageUrl);
+  console.log(replyData.imageUrl[0]);
+
+  if (replyData.imageUrl) {
+    formData.set('imageUrl', replyData.imageUrl[0]);
+  }
+
+  return formData;
 };
