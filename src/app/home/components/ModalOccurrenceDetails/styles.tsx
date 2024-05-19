0f241a606eae4e7f6e81368d268192969107617c
Guilherme Garcia <garciaguig@gmail.com>
2024-05-19 10:49:10 -0300

    feat: Ajusta altura de ModalOccurrenceDetails para diferentes telas

--- src/app/home/components/ModalOccurrenceDetails/styles.tsx
+++ src/app/home/components/ModalOccurrenceDetails/styles.tsx
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
-//
+
 export const ModalContainer = styled.section`
   align-content: start;
   background-color: white;
   border: 2px solid #000;
   box-shadow: 24px;
   display: grid;
-  height: 1050px;
+  height: auto;
   left: 50%;
+  max-height: 90vh;
   overflow-y: auto;
   padding: 1.5rem;
   position: absolute;
   row-gap: 1.5rem;
-  top: 45%;
+  top: 50%;
   transform: translate(-50%, -50%);
   width: 700px;
 
