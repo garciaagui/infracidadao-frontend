1abac66ddae29e9ab3ad2de2628e9609442cfccf
Guilherme Garcia <garciaguig@gmail.com>
2024-05-22 17:47:24 -0300

    Commit inicial - feat: componente Loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { FileField, Textarea, TextField } from './Form';
 import Header from './Header';
+import Loading from './Loading';
 import Notification from './Notification';
 
-export { FileField, Header, Notification, Textarea, TextField };
+export { FileField, Header, Loading, Notification, Textarea, TextField };
