65c010490c628782ccf2f66a75de1cd5a1a6aa2e
Guilherme Garcia <garciaguig@gmail.com>
2024-05-22 17:54:19 -0300

    feat: Implementa componente Loading na Login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { TextField } from '@/components/Form';
-import Notification from '@/components/Notification';
+import { Loading, Notification, TextField } from '@/components';
 import theme from '@/styles/theme';
 import { NOTIFICATION_INITIAL_STATE } from '@/utils/constants';
 import { NotificationType } from '@/utils/types';
@@ -11,12 +10,14 @@ import { signIn, SignInResponse } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import * as Styled from './styles';
+import { Button, Form, Link, Main } from './styles';
 import { loginSchema } from './utils/schemas';
 import { LoginType } from './utils/types';
 
 export default function Login() {
   const [notification, setNotification] = useState<NotificationType>(NOTIFICATION_INITIAL_STATE);
+  const [loading, setLoading] = useState(false);
+
   const router = useRouter();
   const loginForm = useForm<LoginType>({
     resolver: zodResolver(loginSchema),
@@ -29,6 +30,8 @@ export default function Login() {
   };
 
   const login = async (data: LoginType) => {
+    setLoading(true);
+
     const response = (await signIn('credentials', {
       ...data,
       redirect: false,
@@ -42,24 +45,29 @@ export default function Login() {
     } else if (!ok && error) {
       setNotification({ isOpen: true, message: error, severity: 'error' });
     }
+    setLoading(false);
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Styled.Main>
+      <Main>
         <h1>Login</h1>
         <FormProvider {...loginForm}>
-          <Styled.Form onSubmit={handleSubmit(login)} noValidate>
+          <Form onSubmit={handleSubmit(login)} noValidate>
             <TextField label="E-mail" name="email" type="email" />
             <TextField label="Senha" name="password" type="password" />
-            <Styled.Button type="submit" variant="contained" color="primary" size="large">
-              Entrar
-            </Styled.Button>
-          </Styled.Form>
+            {!loading ? (
+              <Button type="submit" variant="contained" color="primary" size="large">
+                Entrar
+              </Button>
+            ) : (
+              <Loading />
+            )}
+          </Form>
         </FormProvider>
-        <Styled.Link href={'/register'}>Não tem conta? Cadastre-se</Styled.Link>
+        <Link href={'/register'}>Não tem conta? Cadastre-se</Link>
         <Notification closeNotification={closeNotification} {...notification} />
-      </Styled.Main>
+      </Main>
     </ThemeProvider>
   );
 }
