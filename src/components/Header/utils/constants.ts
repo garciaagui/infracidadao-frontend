4c00f55c5014956239203129210b174811ff7ec4
Guilherme Garcia <garciaguig@gmail.com>
2024-05-29 15:41:39 -0300

    feat: Cria lógica p/ renderizar elementos diferentes no Header para usuários admin

---
+++ src/components/Header/utils/constants.ts
@@ -0,0 +1,10 @@
+export const ADMIN_ELEMENTS = [
+  { href: '/home', label: 'Início' },
+  { href: '/admin', label: 'Área Administrador' },
+];
+
+export const DEFAULT_ELEMENTS = [
+  { href: '/home', label: 'Início' },
+  { href: '/home', label: 'Minhas reclamações' },
+  { href: '/home', label: 'Perfil' },
+];
