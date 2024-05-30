80e0ebd02d7abe74ff0eaa24c98bd90b4b71891b
Guilherme Garcia <garciaguig@gmail.com>
2024-05-30 18:11:33 -0300

    refactor: Implementa novo componente Card em Admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,9 +3,9 @@
 import { Header } from '@/components';
 import theme from '@/styles/theme';
 import PersonAddAltSharpIcon from '@mui/icons-material/PersonAddAltSharp';
-import { Card, CardActionArea, CardContent, Stack, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import { Link, Main } from './styles';
+import { Card } from './_components';
+import { Main } from './styles';
 
 export default function Admin() {
   return (
@@ -13,21 +13,12 @@ export default function Admin() {
       <Header />
       <Main>
         <h1>Área do Administrador</h1>
-        <Card sx={{ width: 275 }} variant="outlined">
-          <CardActionArea>
-            <Link href="/admin/new-user">
-              <CardContent>
-                <Stack spacing={1}>
-                  <Stack alignItems="center" direction="row" spacing={1}>
-                    <PersonAddAltSharpIcon fontSize="small" />
-                    <h3>Novo usuário</h3>
-                  </Stack>
-                  <Typography>Adicione usuários do tipo employee e admin</Typography>
-                </Stack>
-              </CardContent>
-            </Link>
-          </CardActionArea>
-        </Card>
+        <Card
+          href="/admin/new-user"
+          icon={<PersonAddAltSharpIcon />}
+          label="Adicione usuários do tipo employee e admin"
+          title="Novo usuário"
+        />
       </Main>
     </ThemeProvider>
   );
