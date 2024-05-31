e70a8c03de4edb8962a16fb9ac9e661e884463e8
Guilherme Garcia <garciaguig@gmail.com>
2024-05-31 12:02:37 -0300

    feat: Add novo componente de Form - SelectField

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-import { FileField, Textarea, TextField } from './Form';
+import { FileField, SelectField, Textarea, TextField } from './Form';
 import Header from './Header';
 import Loading from './Loading';
 import Notification from './Notification';
 
-export { FileField, Header, Loading, Notification, Textarea, TextField };
+export { FileField, Header, Loading, Notification, SelectField, Textarea, TextField };
